89e3267c6011af6818eb8b3e78998c619456a2a4
HaltingColt522 <l.frotschi@web.de>
2026-09-07 20:30:26 +0200

    feat: add base revision and framebuffer checks
    add kmain() with noreturn, checks for base revision and framebuffer
    response, halting with hcf() if either fails. fix marker macro name typo
    (REQUEST -> REQUESTS)

--- src/kernel.c
+++ src/kernel.c
@@ -1,4 +1,5 @@
 #include "limine.h"
+#include <stddef.h>
 
 __attribute__((used, section(".limine_requests")))
 static volatile uint64_t limine_base_revision[] = LIMINE_BASE_REVISION(6);
@@ -10,7 +11,25 @@ static volatile struct limine_framebuffer_request framebuffer_request = {
 };
 
 __attribute__((used, section(".limine_requests")))
-static volatile uint64_t limine_request_start_marker[] = LIMINE_REQUEST_START_MARKER;
+static volatile uint64_t limine_request_start_marker[] = LIMINE_REQUESTS_START_MARKER;
 
 __attribute__((used, section(".limine_requests")))
-static volatile uint64_t limine_request_end_marker[] = LIMINE_REQUEST_END_MARKER;
+static volatile uint64_t limine_request_end_marker[] = LIMINE_REQUESTS_END_MARKER;
+
+/* halt and catch fire */
+__attribute__((noreturn))
+static void hcf() {
+	for(;;) {
+		__asm__ volatile("hlt");
+	}
+}
+
+__attribute__((noreturn))
+void kmain() {
+	if(!LIMINE_BASE_REVISION_SUPPORTED(limine_base_revision)) {hcf();}
+
+	if((framebuffer_request.response == NULL) || 
+		(framebuffer_request.response->framebuffer_count < 1)) {hcf();}
+
+	hcf();
+}
